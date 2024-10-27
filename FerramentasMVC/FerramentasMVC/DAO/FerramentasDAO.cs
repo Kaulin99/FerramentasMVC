27b9b77a8ad3ab974d761ca535b425f5f4a5a240
KAUE DOS SANTOS ANDRADE <082220027@faculdade.cefsa.edu.br>
2024-10-27 17:04:35 -0300

    Criado Model, HelperDAO, ConexaoDAO, e FerramentasDAO ("EnviaParametros","RecebeParametros","CriaLista").

---
+++ FerramentasMVC/FerramentasMVC/DAO/FerramentasDAO.cs
@@ -0,0 +1,53 @@
+﻿using FerramentasMVC.Models;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace FerramentasMVC.DAO
+{
+    public class FerramentasDAO 
+    {
+        private SqlParameter[] EnviaParametros(FerramentasViewModel f)
+        {
+            SqlParameter[] envia = new SqlParameter[3];
+            envia[0] = new SqlParameter("Id", f.Id);
+            envia[1] = new SqlParameter("descricao", f.descricao);
+            envia[2] = new SqlParameter("FabricanteId", f.FabricanteId);
+
+            return envia;
+        }
+
+        private FerramentasViewModel RecebeParametros(DataRow recebe)
+        {
+            FerramentasViewModel f = new FerramentasViewModel();
+            f.Id = Convert.ToInt32(recebe["Id"]);
+            f.descricao = Convert.ToString(recebe["Description"]);
+            f.FabricanteId = Convert.ToInt32(recebe["FabricanteId"]);
+
+            return f;
+        }
+
+        public List<FerramentasViewModel> CriaLista()
+        {
+            List<FerramentasViewModel> lista = new List<FerramentasViewModel>();
+            SqlParameter[] parametro = new SqlParameter[]
+                {
+                    new SqlParameter("Tabela","ferramentas")
+                };
+
+
+            DataTable tabela = HelperDAO.ExecutaProcSelect("spListagem",parametro);
+            
+            foreach (DataRow row in tabela.Rows)
+            {
+                lista.Add(RecebeParametros(row));
+            }
+            return lista;
+        }
+        
+        public void Inserir(FerramentasDAO f)
+        {
+            HelperDAO.ExecutaProc("",null);
+        }
+    }
+}
---
+++ FerramentasMVC/FerramentasMVC/Models/FerramentasViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace FerramentasMVC.Models
+{
+    public class FerramentasViewModel
+    {
+        public int Id { get; set; }
+        public string descricao { get; set; }
+        public int FabricanteId { get; set; }
+    }
+}
